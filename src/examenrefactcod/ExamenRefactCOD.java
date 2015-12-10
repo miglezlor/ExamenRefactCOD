40cdbcfeb1527ed1b635ab85a4071b9072246d92
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff05.danielcastelao.org>
2015-12-10 09:05:05 +0100

    Metodos introducidos

--- src/examenrefactcod/ExamenRefactCOD.java
+++ src/examenrefactcod/ExamenRefactCOD.java
@@ -11,53 +11,66 @@ public class ExamenRefactCOD {
     public static void main(String[] args) {
         int digitos = 3;
         int numdigitos = 0;
-        if (digitos <= 0) {
-            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        }
+        MetodoIntroducir(digitos);
         for (int j = 1; j <= 99999; j++) {
             int aux = j;
 
             int cont = 0;
 
-            while (aux != 0) {
-                aux = aux / 10;
-                cont++;
-            }
+            cont = MetodoWhileCont(aux, cont);
             numdigitos = cont;
 
-            if (numdigitos == digitos) {
-                if (j < 4) {
-                    variableBooleana = true;
+            MetodoIfs(numdigitos, digitos, j);
+        }
+    }
+
+    public static int MetodoWhileCont(int aux, int cont) {
+        while (aux != 0) {
+            aux = aux / 10;
+            cont++;
+        }
+        return cont;
+    }
+
+    public static void MetodoIntroducir(int digitos) {
+        if (digitos <= 0) {
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        }
+    }
+
+    public static void MetodoIfs(int numdigitos, int digitos, int j) {
+        if (numdigitos == digitos) {
+            if (j < 4) {
+                variableBooleana = true;
+            } else {
+                if (j % 2 == 0) {
+                    variableBooleana = false;
                 } else {
-                    if (j % 2 == 0) {
-                        variableBooleana = false;
-                    } else {
-                        int cont1 = 0;
-                        int i1 = 1;
-                        int z = (j - 1) / 2;
-                        if (z % 2 == 0) {
-                            z--;
-                        }
+                    int cont1 = 0;
+                    int i1 = 1;
+                    int z = (j - 1) / 2;
+                    if (z % 2 == 0) {
+                        z--;
+                    }
 
-                        while (i1 <= z) {
-                            if (j % i1 == 0) {
-                                cont1++;
-                            }
-                            i1 += 2;
-                            if (cont1 == 2) {
-                                i1 = z + 1;
-                            }
+                    while (i1 <= z) {
+                        if (j % i1 == 0) {
+                            cont1++;
                         }
-
-                        if (cont1 == 1) {
-                            variableBooleana = true;
+                        i1 += 2;
+                        if (cont1 == 2) {
+                            i1 = z + 1;
                         }
                     }
-                }
 
-                if (variableBooleana == true) {
-                    System.out.println(j);
+                    if (cont1 == 1) {
+                        variableBooleana = true;
+                    }
                 }
+            }
+
+            if (variableBooleana == true) {
+                System.out.println(j);
             }
         }
     }
